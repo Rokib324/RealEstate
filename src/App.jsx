fe66a6cc04f9683889391619871d7839b792b68f
Rokibul Islam <rokibulislam@Rokibuls-MacBook-Pro.local>
2025-08-02 01:48:24 +0600

    implement contact form with react toastyfi

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,12 @@ import Projects from './components/Projects'
 import Testimonials from './components/Testimonials'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 const App = () => {
   return (
     <div className='w-full overflow-hidden'>
+      <ToastContainer />
       <Header />
       <About />
       <Projects />
